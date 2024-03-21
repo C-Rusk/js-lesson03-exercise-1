86e302c66c880d7c4815cb0142aadb164d9ef77d
Cassandra Rusk <149620042+C-Rusk@users.noreply.github.com>
2024-03-21 14:05:09 -0500

    JavaScript lesson 3 exercise 1

---
+++ script.js
@@ -0,0 +1,10 @@
+var stringNumber = "12";
+var numericalNumber = 12;
+
+if (stringNumber === numericalNumber) {
+  console.log("The values and types are equal.");
+} else if (stringNumber == numericalNumber) {
+  console.log("The values are equal.");
+} else {
+  console.log("The values are not equal.");
+}
